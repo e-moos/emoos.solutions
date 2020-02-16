540c77abb4f1d2307c1988d20eae3a0b8546040a
Anton Herzog <info@antonherzog.com>
2020-02-16 09:10:39 +0100

    Delivery images as static files

--- src/EMoos.Server/Startup.fs
+++ src/EMoos.Server/Startup.fs
@@ -1,14 +1,17 @@
 namespace EMoos.Server
 
+open Bolero
+open Bolero.Remoting.Server
+open Bolero.Templating.Server
+open EMoos
 open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Authentication.Cookies
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.StaticFiles
 open Microsoft.Extensions.DependencyInjection
-open Bolero
-open Bolero.Remoting.Server
-open EMoos
-open Bolero.Templating.Server
+open Microsoft.Extensions.FileProviders
+open System.IO
 
 type Startup() =
 
@@ -30,10 +33,18 @@ type Startup() =
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        let provider = FileExtensionContentTypeProvider()
+        provider.Mappings.[".fsx"] <- "text/x-fsharp"
         app
             .UseAuthentication()
             .UseRemoting()
-            .UseStaticFiles()
+            .UseStaticFiles(
+            StaticFileOptions(
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(
+                        Path.GetDirectoryName(Directory.GetCurrentDirectory()),
+                        "EMoos.Client", "wwwroot")),
+                ContentTypeProvider = provider))
             .UseRouting()
             .UseClientSideBlazorFiles<Client.Main.MyApp>()
             .UseEndpoints(fun endpoints ->
